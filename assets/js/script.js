2c344bf1d48ccb43af5a0ea3fe2edb5601db939a
Semih Şahinoğlu <semihgtt11@gmail.com>
2025-07-01 19:49:54 +0300

    01.07.2025 calisildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@
 // Mobile Nav Toggle
 const navbar = document.querySelector(".navbar");
 const navToggler = document.querySelector(".nav-open-btn");
+const header = document.querySelector(".header");
 
 navToggler.addEventListener("click", function () {
   navbar.classList.toggle("active");
+  header.classList.toggle("nav-active");
 });
 
 // Header Toggle
-const header = document.querySelector(".header");
 window.addEventListener("scroll", function () {
   header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
 });
